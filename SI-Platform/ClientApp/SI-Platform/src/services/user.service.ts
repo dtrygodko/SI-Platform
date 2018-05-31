eb77063079e7933c3be7aca0a9d18138696be54d
YAro <carlend94@ukr.net>
2018-05-31 23:31:24 +0300

    added service + main users page

---
+++ SI-Platform/ClientApp/SI-Platform/src/services/user.service.ts
@@ -0,0 +1,31 @@
+import {Injectable, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class UserService implements OnInit {
+
+  constructor(public http: HttpClient) {
+  }
+
+  ngOnInit() {
+  }
+
+
+  getUsers(): Observable<any> {
+    return this.http.get('/api/users')
+      .map((res: Response) => {
+        return res;
+      })
+  }
+
+  addUser(user: any): Observable<any> {
+    return this.http.post('/api/addUser', {username: user.username, password: user.password})
+      .map((res: Response) => {
+        return res;
+      })
+  }
+
+
+}
